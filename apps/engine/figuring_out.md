f07c2eaa098de8e384f0a924a6bd4c764c8a2523
Piyush Raj <piyushraj26102004@gmail.com>
2026-05-18 20:32:55 +0530

    added fn in orderbook.rs

---
+++ apps/engine/figuring_out.md
@@ -0,0 +1,9 @@
+engine architecture
+
+1. accept an event  
+2. validate the event - market checks, price checks 
+3. match or cancel the order
+4. update the in-memory orderbook
+5. emit the msg
+
+
--- apps/engine/src/book/orderbook.rs
+++ apps/engine/src/book/orderbook.rs
@@ -1,10 +1,9 @@
 use std::collections::{BTreeMap, VecDeque};
 use crate::order::Order;
 use crate::types::Price;
+use crate::types::Side;
 
 pub struct OrderBook {
-    /* 150$, ["piyush", "rishi"]*/
-
     asks: BTreeMap<Price, VecDeque<Order>>, 
     bids: BTreeMap<Price, VecDeque<Order>>,
     next_order_id: u64,
@@ -18,4 +17,29 @@ impl OrderBook {
             next_order_id: 1,
         }
     }
+
+     pub fn add_as_resting_order(&mut self, order: Order) {
+        let side = match order.side {
+            Side::Buy => &mut self.bids,
+            Side::Sell => &mut self.asks
+        };
+
+        if side.contains_key(&order.price) {
+            let queue = side.get_mut(&order.price).unwrap();
+            queue.push_back(order);
+        } else {
+            let price = order.price;
+            let mut queue = VecDeque::new();
+            queue.push_back(order);
+            side.insert(price, queue);
+        }
+    }
+
+     pub fn get_best_ask(&self) {
+     }
+
+     pub fn get_best_bid(&self) {
+     }
+    
+
 }
